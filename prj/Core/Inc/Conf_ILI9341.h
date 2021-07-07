278ff2f1e1d340990f39bde130b1b9bc71046225
ActivePeter <1020401660@qq.com>
2021-07-08 02:40:50 +0800

    s

---
+++ prj/Core/Inc/Conf_ILI9341.h
@@ -0,0 +1,6 @@
+#ifndef Conf_ILI9341_h_____
+#define Conf_ILI9341_h_____
+#define Conf_ILI9341_CS GPIOB, GPIO_PIN_10
+#define Conf_ILI9341_DC GPIOB, GPIO_PIN_11
+#define Conf_ILI9341_RST GPIOB, GPIO_PIN_12
+#endif
