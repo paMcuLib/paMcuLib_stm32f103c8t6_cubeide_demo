05735b21e33d8e6f59fb3144391fb8e2991ccad8
ActivePeter <1020401660@qq.com>
2021-07-09 00:13:51 +0800

    vscode cpp setting for stm32 related

--- prj/Core/Src/MainApp.cpp
+++ prj/Core/Src/MainApp.cpp
@@ -2,7 +2,7 @@
 // #include "freertos/FreeRTOS.h"
 // #include "freertos/task.h"
 
-// #include "paMods/display/SSD1306/pa_oled.h"
+#include "paMods/display/SSD1306/pa_oled.h"
 // #include "paCoreHead/paIIC.h"
 // #include "paCoreHead/paBase.h"
 #include "paCoreInc/drv/paSPI.h"
@@ -18,13 +18,13 @@ App app;
 
 void mainAppEntry()
 {
-    _G_paSPI.init(0);
-    app.lcd.init(0, pa_ILI9341::Rotation_HORIZONTAL_1);
+    // _G_paSPI.init(0);
+    // app.lcd.init(0, pa_ILI9341::Rotation_HORIZONTAL_1);
 
-    // _G_paIIC.init(0);
-    // OLED_Init(0);
+    _G_paIIC.init(0);
+    OLED_Init(0);
     // OLED_On();
-    // OLED_ShowString(0, 0, "121", 12);
+    OLED_ShowString(0, 0, "121", 12);
     while (1)
     {
         // ESP_LOGI("App", "hello world!");
